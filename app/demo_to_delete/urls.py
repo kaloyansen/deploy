3f7bef48f0d20a1d00aadbaeb9dd9bc52f346cc5
kaloyansen <kalojanssen@gmail.com>
2021-07-18 18:56:22 +0200

    auto-commit by Kaloyan KRASTEV kaloyansen@gmail.com https://freeshell.de/morla

--- app/demo_to_delete/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-	path("", views.demo, name="demo"),
-	path("bg/", views.bg, name="bg"),
-	path("<int:pk>/", views.printer, name="printer")
-]
