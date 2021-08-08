4f8ce1702d8338d802c06c464cce503a92d3fc57
kaloyansen <kalojanssen@gmail.com>
2021-08-08 22:46:04 +0200

    auto-commit by Kaloyan KRASTEV kaloyansen@gmail.com https://freeshell.de/morla

--- app/app/views.py
+++ app/app/views.py
@@ -11,6 +11,7 @@ def index(request):
 	request.session['num_visits'] = num_visits + 1
 	context = {
 		'title': 'développeur back-end',
+		'title_en': 'backend developer',
 		'image': '/img/kalo.png',
 		'slide': '/img/slideshow.gif',
 		'apirest': '/img/800x600/api.rest.png',
@@ -27,7 +28,7 @@ def index(request):
 		'serverless': '/img/800x600/serverless.png',
 		'web': '/img/800x600/web.png',
 		'about': 'conception d\'applications, développement back-end, analyse de données scientifique et visualisation en-ligne',
-		'about_en': 'software designer, developper back-end, scientifique data analysis and visualisation online',
+		'about_en': 'software designer, backend developer, scientific data analysis and visualisation online',
 		'lange': 'python, C++, java, perl, fortran, c, php, javascript',
 		'techno': 'django, pandas, gnuplot, emacs, oracle, mysql, sql*loader, symfony, vue.js, react.js, angular, react',
 		'num_visits': num_visits
