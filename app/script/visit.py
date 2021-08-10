84f73c666a85618b4bbed01b87b6d46488c0f358
kaloyansen <kalojanssen@gmail.com>
2021-08-10 23:46:35 +0000

    142.93.171.130

--- app/script/visit.py
+++ app/script/visit.py
@@ -14,10 +14,14 @@ visifilter = Visitor.objects.exclude(message='')
 print (visifilter.count(), ' messages')
 for v in visifilter: print (v.message)
 
-visifilter = Visitor.objects.filter(message='').filter(voted=False)
+visifilter = Visitor.objects.filter(message='').filter(voted=False).filter(code=0)
 print (visifilter.count(), ' robots')
 for v in visifilter:
 	print (v.code)
-	#v.delete()
+	v.delete()
+
+
+for v in visit:
+	print (v, v.code, v.voted, v.message)
 
 
