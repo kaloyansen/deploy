429a7f0901c2f04114a4226b8858ea7858208f0f
kaloyansen <kalojanssen@gmail.com>
2021-09-19 13:14:47 +0200

    auto-commit by Kaloyan KRASTEV kaloyansen@gmail.com https://freeshell.de/morla

--- app/app/linked.py
+++ app/app/linked.py
@@ -24,49 +24,51 @@ def tothetop(style = 'info'):
 	""".format(style, _("ToTheTop"), _("ToTheTop"))
 
 
-def linked(title, color, url, label):
-	x = '<a title = "{}"\
-	        class = "btn btn-outline-{} btn-lg rounded-circle"\
-	        href = "{}">{}</a>'
-	return x.format(title, color, url, label)
-
-
-def linkedin():
+def linked_news():  return linked(_("News"),     'news/',      'primary')
+def linked_work():  return linked(_("Work"),     'work/',      'info')
+def linked_about(): return linked(_("ThisPage"), 'work/3/',    'success')
+def linked_bg():    return linked(_("PlotBg"),   'memo/bg/',   'info', _("Bulgarian"))
+def linked_demo():  return linked(_("PlotLang"), 'memo/demo/', 'danger')
+def linked_sun():   return linked(_("PlotSun"),  'memo/sun/',  'warning')
+def linked(title, url, style, label = False):
+	if not label: label = title
 	return """
-      <a title = "https://www.linkedin.com/in/kaloyan-k-krastev"
-         class = "btn-outline-info"
-         href = "https://www.linkedin.com/in/kaloyan-k-krastev/">
-        <img src = "https://www.linkedin.com/favicon.ico"
-             height = "20mm"
-             alt = "linkedin"></a>
-	"""
+<a title = "{}"
+   class = "btn btn-outline-{} btn-lg rounded-circle"
+   href = "{}">{}</a>
+	""".format(title, style, url, label)
 
 
-def github():
+def python(): return limage('python',
+							'https://www.python.org',
+							'/static/svg/python.svg')
+def django(): return limage('django',
+							'https://www.djangoproject.com',
+							'/static/svg/django.svg')
+def mycv(): return limage('curriculum vitæ',
+						  '/cv',
+						  '/static/svg/cv.svg')
+def stackoverflow(): return limage('stackoverflow',
+								   'https://stackoverflow.com/users/3566444/morla',
+								   '/static/ico/stackoverflow.ico')
+def linkedin(): return limage('linkedin',
+							  'https://www.linkedin.com/in/kaloyan-k-krastev',
+							  '/static/ico/linkedin.ico')
+def github(): return limage('github',
+							'https://github.com/kaloyansen/deploy.git',
+							'/static/svg/github.svg')
+#							'/static/ico/github.ico')
+def digitalocean(size = 24): return limage('digitalocean',
+										   'https://www.digitalocean.com/?refcode=ff8b99f4b98b&utm_campaign=Referral_Invite&utm_medium=Referral_Program&utm_source=badge',
+										   '/static/svg/digital.svg',
+										   '{}mm'.format(size))
+def limage(title, url, source, height = '20mm', style = 'info'):
 	return """
-      <a title = "https://github.com/kaloyansen/deploy"
-         class = "btn-outline-warning"
-         href = "https://github.com/kaloyansen/deploy.git">
-        <img src = "https://www.github.com/favicon.ico"
-             height = "20mm"
-             alt = "https://github.com/kaloyansen/deploy"></a>
-	"""
-
-
-def digitalocean(request, size = 24):
-	return """
-	<a title = "digitalocean.com/kaloyansen"
-	   class = "btn-outline-info"
-       href = "https://www.digitalocean.com/?refcode=ff8b99f4b98b&utm_campaign=Referral_Invite&utm_medium=Referral_Program&utm_source=badge">
-      <img src = "https://web-platforms.sfo2.cdn.digitaloceanspaces.com/WWW/Badge%201.svg"
-           height = "{}mm"
-           alt = "DigitalOcean Referral Badge" /></a>
-	""".format(size)
-
-
-def linked_news(): return linked(_("News"), "primary", "news/", _("News"))
-def linked_work(): return linked(_("Work"), "info", "work/", _("Work"))
-def linked_about(): return linked(_("ThisPage"), "success", "work/3/", _("ThisPage"))
-def linked_bg(): return linked(_("PlotBg"), "info", "memo/bg/", _("Bulgarian"))
-def linked_demo(): return linked(_("PlotLang"), "danger", "memo/demo/", _("PlotLang"))
-def linked_sun(): return linked(_("PlotSun"), "warning", "memo/sun/", _("PlotSun"))
+<a title = "automatic generation"
+   class = "btn-outline-{}"
+   href = "{}">
+  <img title = "{}"
+	   src = "{}"
+       height = "{}"
+       alt = "image not found"></a>
+	""".format(style, url, title, source, height)
