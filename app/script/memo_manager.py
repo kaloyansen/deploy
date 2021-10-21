dd213a97d970f72a1a748b2760a0732da5436da0
kaloyansen <kalojanssen@gmail.com>
2021-10-21 21:48:40 +0200

    auto-commit by Kaloyan KRASTEV kaloyansen@gmail.com https://freeshell.de/morla

--- app/script/memo_manager.py
+++ app/script/memo_manager.py
@@ -3,13 +3,13 @@ from memo.models import Child, Prog, Parent
 
 p, cr = Parent.objects.get_or_create(name = "bg")
 danni = {
-	'db': {'code1': 27, 'code2': 34, 'code3': 24, 'color': "blue", 'mother': p},
-	'itn': {'code1': 51, 'code2': 65, 'code3': 38, 'color': "cyan",	'mother': p},
-	'pp': {'code1': 0, 'code2': 0, 'code3': 39, 'color': "yellow", 'mother': p},
+	'db': {'code1': 27, 'code2': 34, 'code3': 32, 'color': "blue", 'mother': p},
+	'itn': {'code1': 51, 'code2': 65, 'code3': 34, 'color': "cyan",	'mother': p},
+	'pp': {'code1': 0, 'code2': 0, 'code3': 38, 'color': "yellow", 'mother': p},
 	'imv': {'code1': 14, 'code2': 13, 'code3': 0, 'color': "green", 'mother': p},
-	'bsp': {'code1': 43, 'code2': 36, 'code3': 51, 'color': "red", 'mother': p},
-	'dps': {'code1': 30, 'code2': 29, 'code3': 23, 'color': "violet", 'mother': p},
-	'gerb': {'code1': 75, 'code2': 63, 'code3': 65, 'color': "black", 'mother': p}}
+	'bsp': {'code1': 43, 'code2': 36, 'code3': 43, 'color': "red", 'mother': p},
+	'dps': {'code1': 30, 'code2': 29, 'code3': 30, 'color': "violet", 'mother': p},
+	'gerb': {'code1': 75, 'code2': 63, 'code3': 63, 'color': "black", 'mother': p}}
 
 for da in danni.keys():
 	Child.objects.update_or_create(name = da,
@@ -19,25 +19,27 @@ p.save()
 
 pp, cr = Parent.objects.get_or_create(name = "prog")
 panni = {
-	0: {'code1': 1, 'color': "black", 'mother': pp},
-	1: {'code1': 1, 'color': "white", 'mother': pp},
-	2: {'code1': 14, 'color': "blue", 'mother': pp},
-	3: {'code1': 11, 'color': "red", 'mother': pp},
-	4: {'code1': 7, 'color': "green", 'mother': pp},
-	5: {'code1': 5, 'color': "magenta", 'mother': pp},
-	6: {'code1': 4, 'color': "cyan", 'mother': pp},
-	7: {'code1': 3, 'color': "violet", 'mother': pp},
-	8: {'code1': 3, 'color': "yellow", 'mother': pp},
-	9: {'code1': 6, 'color': "orange", 'mother': pp},
-	10: {'code1': 3, 'color': "brown", 'mother': pp}}
+	0: {'code1': 1, 'color': "black", 'mother': pp},# unknown
+	1: {'code1': 1, 'color': "white", 'mother': pp},# other
+	2: {'code1': 61818, 'color': "blue", 'mother': pp},# python
+	3: {'code1': 38018, 'color': "red", 'mother': pp},# javascript
+	4: {'code1': 65986, 'color': "green", 'mother': pp},# java
+	5: {'code1': 27521, 'color': "magenta", 'mother': pp},# c#
+	6: {'code1': 36798, 'color': "cyan", 'mother': pp},# c++
+	7: {'code1': 3, 'color': "violet", 'mother': pp},# r
+	8: {'code1': 3, 'color': "yellow", 'mother': pp},# c
+	9: {'code1': 16890, 'color': "orange", 'mother': pp},# php
+	10: {'code1': 3, 'color': "brown", 'mother': pp}}# perl
 
-if cr:
+if False: # False to disable update/destroy the database
+	# True to enable update/destroy it
+	print('warning -> update database')
 	for pa in panni.keys():
 		Prog.objects.update_or_create(name = pa,
-									  defaults = panni[pa])
+   									  defaults = panni[pa])
 	pp.save()
-else:
-	print('not touch prog once created to prevent user data lost')
+else: print('keep safe database -> edit the script to update')
+
 
 avec = True
 for prog in Prog.objects.all():
